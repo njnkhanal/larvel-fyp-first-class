3096645487745555ee022a71ad545f005d0114df
Njn khanal <45514077+njnkhanal@users.noreply.github.com>
2023-01-22 06:54:34 +0545

    use images in laravel project

--- fyp-class/resources/views/welcome.blade.php
+++ fyp-class/resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <div class="container">
             <div class="jumbotron">
                 <h1 class="display-3">Homepage</h1>
+                <img src="{{ asset('frontend/backgroud.jpg') }}" class="mt-4" width="100%" alt="">
             </div>
             <button class="btn-test">Test Css</button>
         </div>
